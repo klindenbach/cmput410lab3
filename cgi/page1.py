2927fd699640301e44ffe73fb8c19410d2b9b2ce
Konrad lindenbach <klindenb@ualberta.ca>
2015-01-27 09:17:40 -0700

    Added checboxes, links, and an arbitrary feature

--- cgi/page1.py
+++ cgi/page1.py
@@ -6,6 +6,9 @@ form = cgi.FieldStorage()
 
 bday   = form.getvalue("birthdate")
 hobby  = form.getvalue("hobby")
+home  = form.getvalue("home")
+form1 = form.getvalue("form1")
+form2 = form.getvalue("form2")
 
 print """Content-type: text/html
 
@@ -21,13 +24,21 @@ if bday:
     print "<p> Your birthday is %s</p>\n" % bday
 if hobby:
     print "<p> Your hobby is %s</p>\n" % hobby
+if form1:
+    print '<a href="/cgi/page1.py">Form 1</a>\n'
+if form2:
+    print '<a href="/cgi/page2.py">Form 1</a>\n'
+if home:
+    print '<a href="/">Home</a>\n'
 
-print """
+print """ <br /><br />
+    <marquee>Arbitrary feature</marquee>
     <form method="post" action="page2.py">
-        First Name: <input type="text" name="first" />
-        <br/>
-        Family Name: <input type="text" name="family" />
-        <br />
+        First Name: <input type="text" name="first" /> <br />
+        Family Name: <input type="text" name="family" /> <br />
+        <input type="checkbox" name="form1" /> Show Form 1<br />
+        <input type="checkbox" name="form2" /> Show Form 2<br />
+        <input type="checkbox" name="home" /> Show Home<br />
         <input type="submit" value="Submit">
     </form>
 </body>
--- cgi/page2.py
+++ cgi/page2.py
@@ -6,6 +6,9 @@ form = cgi.FieldStorage()
 
 first = form.getvalue("first")
 last  = form.getvalue("family")
+home  = form.getvalue("home")
+form1 = form.getvalue("form1")
+form2 = form.getvalue("form2")
 
 print """Content-type: text/html
 
@@ -18,14 +21,22 @@ print """Content-type: text/html
 <body> """
 
 if first and last:
-    print " <p> Hello %s %s <p>\n" % (first, last)
+    print "<p> Hello %s %s <p>\n" % (first, last)
+if form1:
+    print '<a href="/cgi/page1.py">Form 1</a>\n'
+if form2:
+    print '<a href="/cgi/page2.py">Form 1</a>\n'
+if home:
+    print '<a href="/">Home</a>\n'
 
-print """
+print """<br /><br />
+    <marquee>Arbitrary feature</marquee>
     <form method="post" action="page1.py">
-        Birthdate: <input type="text" name="birthdate" />
-        <br/>
-        Main Hobby: <input type="text" name="hobby" />
-        <br />
+        Birthdate: <input type="text" name="birthdate" /><br/>
+        Main Hobby: <input type="text" name="hobby" /><br />
+        <input type="checkbox" name="form1" /> Show Form 1<br />
+        <input type="checkbox" name="form2" /> Show Form 2<br />
+        <input type="checkbox" name="home" /> Show Home<br />
         <input type="submit" value="Submit">
     </form>
 </body>
